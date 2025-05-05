78baed4cd8a84b66decb81b19fec9e823de7c262
Emon-Ahmed <technicalboyemon@gmail.com>
2025-05-06 00:28:23 +0600

    feat: create blog post api & admin dashboard

---
+++ src/app/api/blogs/route.js
@@ -0,0 +1,41 @@
+import { ConnectDB } from "@/lib/config/db";
+import BlogModel from "@/lib/models/BlogModel";
+const { NextResponse } = require("next/server");
+import { writeFile } from "fs/promises";
+
+const LoadDB = async () => {
+  await ConnectDB();
+};
+
+LoadDB();
+
+export const GET = async (request) => {
+  console.log("Blog GET Hit");
+  return NextResponse.json({ message: "API Working" });
+};
+
+export const POST = async (request) => {
+  const formData = await request.formData();
+  const timestamp = Date.now();
+
+  const image = formData.get("image");
+  const imageByteData = await image.arrayBuffer();
+  const buffer = Buffer.from(imageByteData);
+  const path = `./public/${timestamp}_${image.name}`;
+  await writeFile(path, buffer);
+  const imgUrl = `/${timestamp}_${image.name}`;
+
+  const blogData = {
+    title: `${formData.get("title")}`,
+    description: `${formData.get("description")}`,
+    category: `${formData.get("category")}`,
+    author: `${formData.get("author")}`,
+    image: `${imgUrl}`,
+    author_img: `${formData.get("author_img")}`,
+  };
+
+  await BlogModel.create(blogData);
+  console.log("Blog saved");
+
+  return NextResponse.json({ success: true, message: "Blog Created" });
+};
